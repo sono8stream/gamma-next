120d4c21acf42919edaaad26429068b7f58504c1
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-19 22:31:57 +0900

    Add firebase configs

---
+++ src/app/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhZyqvHbOunErd1N0tcdgqo20vJpD6ms0",
+  authDomain: "gamma-creators.firebaseapp.com",
+  databaseURL: "https://gamma-creators.firebaseio.com",
+  messagingSenderId: "439854475945",
+  projectId: "gamma-creators",
+  storageBucket: "gamma-creators.appspot.com",
+};
