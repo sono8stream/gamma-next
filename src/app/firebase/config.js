3ee630918d3e3955d0094ee31fe8c45c32b500ec
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-22 19:15:43 +0900

    Remove firebase config from github

--- src/app/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAhZyqvHbOunErd1N0tcdgqo20vJpD6ms0",
-  authDomain: "gamma-creators.firebaseapp.com",
-  databaseURL: "https://gamma-creators.firebaseio.com",
-  messagingSenderId: "439854475945",
-  projectId: "gamma-creators",
-  storageBucket: "gamma-creators.appspot.com",
-};
