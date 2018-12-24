3f3968b6115649197d40c84d58574a3c538ba440
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-12-24 16:25:33 +0900

    Update home routing

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -4,7 +4,7 @@ import { Router } from '../../functions/routes';
 export default class Home extends React.Component {
 
   componentDidMount() {
-    Router.pushRoute('/blogs');
+    Router.pushRoute('blogs');
   }
 
   render() {
