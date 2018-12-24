7198a17919756441ee939e922036e4e5676cf8d0
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-12-24 23:59:40 +0900

    Update blog index style

--- src/app/pages/blogs.js
+++ src/app/pages/blogs.js
@@ -14,130 +14,150 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { ButtonBase } from '@material-ui/core';
 
+import colors from '../style/themeColor';
+import { relative } from 'path';
+
 const blogRef = firebaseDB.ref('blogs');
 
 export default class Blog extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            loadCompleted: false,
-            uid: undefined,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loadCompleted: false,
+      uid: undefined,
+    };
+  }
 
-    componentWillMount() {
-        //window.scrollTo(0, 0);
+  componentWillMount() {
+    //window.scrollTo(0, 0);
 
-        blogRef.on("child_added", (snapshot) => {
-            let val = snapshot.val();
-            firebaseDB.ref(`accounts/${val.author}`).once('value', account => {
-                let newArticles = this.state.articles;
-                let name = account.val() ? account.val().name : 'who';
-                let title = val.title;
-                if (val.accessibility !== '公開') {
-                    title += ` (${val.accessibility})`;
-                }
+    blogRef.on("child_added", (snapshot) => {
+      let val = snapshot.val();
+      firebaseDB.ref(`accounts/${val.author}`).once('value', account => {
+        let newArticles = this.state.articles;
+        let name = account.val() ? account.val().name : 'who';
+        let title = val.title;
+        if (val.accessibility !== '公開') {
+          title += ` (${val.accessibility})`;
+        }
 
-                newArticles.push({
-                    id: snapshot.key,
-                    accessibility: val.accessibility,
-                    date: val.date,
-                    time: val.time,
-                    author: val.author,
-                    authorName: name,
-                    preview: val.preview,
-                    title: title,
-                });
-                newArticles.sort((a, b) => {
-                    if (a.date + a.time > b.date + b.time) {
-                        return -1;
-                    }
-                    if (a.date + a.time < b.date + b.time) {
-                        return 1;
-                    }
-                    return 0;
-                });
-                this.setState({
-                    articles: newArticles,
-                    loadCompleted: true,
-                });
-            });
+        newArticles.push({
+          id: snapshot.key,
+          accessibility: val.accessibility,
+          date: val.date,
+          time: val.time,
+          author: val.author,
+          authorName: name,
+          preview: val.preview,
+          title: title,
         });
+        newArticles.sort((a, b) => {
+          if (a.date + a.time > b.date + b.time) {
+            return -1;
+          }
+          if (a.date + a.time < b.date + b.time) {
+            return 1;
+          }
+          return 0;
+        });
+        this.setState({
+          articles: newArticles,
+          loadCompleted: true,
+        });
+      });
+    });
 
-        firebaseAuth().onAuthStateChanged(user => {
-            if (user) {
-                this.setState({ uid: user.uid });
+    firebaseAuth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ uid: user.uid });
+      }
+    });
+  }
+
+  render() {
+    if (!this.state.loadCompleted) {
+      return (
+        <Header text="KawazST Blog" onLoad />
+      );
+    }
+    else {
+      return (
+        <div>
+          <Header text="KawazST Blog" />
+          {(() => {
+            if (this.state.uid) {
+              return (
+                <Grid container spacing={16} justify="flex-start">
+                  <Grid item xs={12}>
+                    <Button color='secondary' variant='outlined'
+                      href={href('blogEdit', { id: 'new' })}>
+                      {'新しく書く'}
+                    </Button>
+                  </Grid>
+                </Grid>
+              );
             }
-        });
+          })()}
+          <Grid container spacing={16}
+            justify="flex-start"
+            alignItems="stretch">
+            {this.state.articles.map((a, i) => {
+              if (a.accessibility === '公開'
+                || (a.accessibility === '限定公開' && this.state.uid)
+                || this.state.uid === a.author) {
+                return (
+                  <Grid item key={i}
+                    xs={12} sm={i == 0 ? 12 : 6} md={i == 0 ? 12 : 4}>
+                    <ButtonBase key={i} href={href('blogShow', { id: a.id })}
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                      }} >
+                      <Card
+                        style={{
+                          width: '100%',
+                          height: '100%'
+                        }}>
+                        <CardContent style={{
+                          backgroundColor: colors.primaryDark,
+                        }}>
+                          <Typography gutterBottom
+                            style={{ color: 'white' ,height:2}}                          >
+                            {`${a.date}   by ${a.authorName}`}
+                          </Typography>
+                        </CardContent>
+                        <CardContent style={{
+                          backgroundColor: colors.primary
+                        }}>
+                          <Typography variant='h5' align='center'
+                            style={{
+                              color: 'white',
+                              fontSize: '120%',
+                            }}>
+                            {a.title}
+                          </Typography>
+                        </CardContent>
+                        <CardMedia
+                          image='https://gamma-creators.firebaseapp.com/icon.png'
+                          style={{ height: 0, paddingTop: '56.25%' }} />
+                        <Divider light/>
+                        <CardContent>
+                          <Typography variant="body1" align='center'>
+                            {a.preview}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </ButtonBase>
+                  </Grid>
+                );
+              }
+            }
+            )}
+          </Grid>
+          <Footer />
+        </div >
+      );
     }
-
-    render() {
-        if (!this.state.loadCompleted) {
-            return (
-                <Header text="KawazST Blog" onLoad />
-            );
-        }
-        else {
-            return (
-                <div>
-                    <Header text="KawazST Blog" />
-                    {(() => {
-                        if (this.state.uid) {
-                            return (
-                                <Grid container spacing={16} justify="flex-start">
-                                    <Grid item xs={12}>
-                                        <Button color='secondary' variant='outlined'
-                                            href={href('blogEdit', { id: 'new' })}>
-                                            {'新しく書く'}
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            );
-                        }
-                    })()}
-                    <Grid container spacing={16}
-                        justify="flex-start"
-                        alignItems="stretch"
-                        style={{ flexGrow: 1 }}>
-                        {this.state.articles.map((a, i) => {
-                            if (a.accessibility === '公開'
-                                || (a.accessibility === '限定公開' && this.state.uid)
-                                || this.state.uid === a.author) {
-                                return (
-                                    <Grid item xs={12} sm={i == 0 ? 12 : 4} key={i}>
-                                        <ButtonBase key={i} href={href('blogShow', { id: a.id })}
-                                            style={{ width: '100%', textTransform: 'none' }} >
-                                            <Card style={{ width: '100%' }}>
-                                                <CardContent>
-                                                    <Typography color='textSecondary' gutterBottom>
-                                                        {`${a.date}   by ${a.authorName}`}
-                                                    </Typography>
-                                                    <Typography variant='h5' align='center'>
-                                                        {a.title}
-                                                    </Typography>
-                                                </CardContent>
-                                                <Divider light />
-                                                <CardMedia
-                                                    image='https://gamma-creators.firebaseapp.com/icon.png'
-                                                    style={{ height: 0, paddingTop: '56.25%' }} />
-                                                <Divider light />
-                                                <CardContent>
-                                                    <Typography variant="body1" align='center'>
-                                                        {a.preview}
-                                                    </Typography>
-                                                </CardContent>
-                                            </Card>
-                                        </ButtonBase>
-                                    </Grid>
-                                );
-                            }
-                        }
-                        )}
-                    </Grid>
-                    <Footer />
-                </div>
-            );
-        }
-    }
+  }
 }
