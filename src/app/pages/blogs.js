db62ac823185f7819386787b18393409e1d6c0a8
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-12-24 22:40:09 +0900

    Add sumbnail of article

--- src/app/pages/blogs.js
+++ src/app/pages/blogs.js
@@ -4,136 +4,140 @@ import { Link,href } from '../../functions/routes';
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Divider from '@material-ui/core/Divider';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { ButtonBase } from '@material-ui/core';
 
 const blogRef = firebaseDB.ref('blogs');
 
 export default class Blog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loadCompleted: false,
-      uid: undefined,
-    };
-  }
-
-  componentWillMount() {
-    //window.scrollTo(0, 0);
-
-    blogRef.on("child_added", (snapshot) => {
-      let val = snapshot.val();
-      firebaseDB.ref(`accounts/${val.author}`).once('value', account => {
-        let newArticles = this.state.articles;
-        let name = account.val() ? account.val().name : 'who';
-        let title = val.title;
-        if (val.accessibility !== '公開') {
-          title += ` (${val.accessibility})`;
-        }
-
-        newArticles.push({
-          id: snapshot.key,
-          accessibility: val.accessibility,
-          date: val.date,
-          time: val.time,
-          author: val.author,
-          authorName: name,
-          preview: val.preview,
-          title: title,
-        });
-        newArticles.sort((a, b) => {
-          if (a.date + a.time > b.date + b.time) {
-            return -1;
-          }
-          if (a.date + a.time < b.date + b.time) {
-            return 1;
-          }
-          return 0;
-        });
-        this.setState({
-          articles: newArticles,
-          loadCompleted: true,
-        });
-      });
-    });
-
-    firebaseAuth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ uid: user.uid });
-      }
-    });
-  }
-
-  render() {
-    if (!this.state.loadCompleted) {
-      return (
-        <Header text="KawazST Blog" onLoad />
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            loadCompleted: false,
+            uid: undefined,
+        };
     }
-    else {
-      return (
-        <div>
-          <Header text="KawazST Blog" />
 
-          <Grid container spacing={16}>
-            {(() => {
-              if (this.state.uid) {
-                return (
-                  <Grid item xs={12}>
-                    <Link route='blogEdit' params={{ id: 'new' }}>
-                      <Button color='secondary' variant='outlined'
-                        href={href('blogEdit', { id: 'new' })}>
-                        新しく書く
-                    </Button>
-                    </Link>
-                  </Grid>
-                );
-              }
-            })()}
-            {this.state.articles.map((a, i) => {
-              if (a.accessibility === '公開'
-                || (a.accessibility === '限定公開' && this.state.uid)
-                || this.state.uid === a.author) {
-                return (
-                  <Grid item xs={12} key={i}>
-                    <Card>
-                      <CardActionArea>
-                        <Link route='blogShow' params={{ id: a.id }}>
-                          <CardContent>
-                            <Typography color="textSecondary" gutterBottom>
-                              {`${a.date}   by ${a.authorName}`}
-                            </Typography>
-                            <Typography variant="h6">
-                              {a.title}
-                            </Typography>
-                            <Divider light />
-                            <br />
-                            <Typography variant="body1">
-                              {a.preview}
-                            </Typography>
-                          </CardContent>
-                        </Link>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                );
-              }
+    componentWillMount() {
+        //window.scrollTo(0, 0);
+
+        blogRef.on("child_added", (snapshot) => {
+            let val = snapshot.val();
+            firebaseDB.ref(`accounts/${val.author}`).once('value', account => {
+                let newArticles = this.state.articles;
+                let name = account.val() ? account.val().name : 'who';
+                let title = val.title;
+                if (val.accessibility !== '公開') {
+                    title += ` (${val.accessibility})`;
+                }
+
+                newArticles.push({
+                    id: snapshot.key,
+                    accessibility: val.accessibility,
+                    date: val.date,
+                    time: val.time,
+                    author: val.author,
+                    authorName: name,
+                    preview: val.preview,
+                    title: title,
+                });
+                newArticles.sort((a, b) => {
+                    if (a.date + a.time > b.date + b.time) {
+                        return -1;
+                    }
+                    if (a.date + a.time < b.date + b.time) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                this.setState({
+                    articles: newArticles,
+                    loadCompleted: true,
+                });
+            });
+        });
+
+        firebaseAuth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ uid: user.uid });
             }
-            )}
-          </Grid>
-          <Footer />
-        </div>
-      );
+        });
     }
-  }
+
+    render() {
+        if (!this.state.loadCompleted) {
+            return (
+                <Header text="KawazST Blog" onLoad />
+            );
+        }
+        else {
+            return (
+                <div>
+                    <Header text="KawazST Blog" />
+                    {(() => {
+                        if (this.state.uid) {
+                            return (
+                                <Grid container spacing={16} justify="flex-start">
+                                    <Grid item xs={12}>
+                                        <Button color='secondary' variant='outlined'
+                                            href={href('blogEdit', { id: 'new' })}>
+                                            {'新しく書く'}
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            );
+                        }
+                    })()}
+                    <Grid container spacing={16}
+                        justify="flex-start"
+                        alignItems="stretch"
+                        style={{ flexGrow: 1 }}>
+                        {this.state.articles.map((a, i) => {
+                            if (a.accessibility === '公開'
+                                || (a.accessibility === '限定公開' && this.state.uid)
+                                || this.state.uid === a.author) {
+                                return (
+                                    <Grid item xs={12} sm={i == 0 ? 12 : 4} key={i}>
+                                        <ButtonBase key={i} href={href('blogShow', { id: a.id })}
+                                            style={{ width: '100%', textTransform: 'none' }} >
+                                            <Card style={{ width: '100%' }}>
+                                                <CardContent>
+                                                    <Typography color='textSecondary' gutterBottom>
+                                                        {`${a.date}   by ${a.authorName}`}
+                                                    </Typography>
+                                                    <Typography variant='h5' align='center'>
+                                                        {a.title}
+                                                    </Typography>
+                                                </CardContent>
+                                                <Divider light />
+                                                <CardMedia
+                                                    image='https://gamma-creators.firebaseapp.com/icon.png'
+                                                    style={{ height: 0, paddingTop: '56.25%' }} />
+                                                <Divider light />
+                                                <CardContent>
+                                                    <Typography variant="body1" align='center'>
+                                                        {a.preview}
+                                                    </Typography>
+                                                </CardContent>
+                                            </Card>
+                                        </ButtonBase>
+                                    </Grid>
+                                );
+                            }
+                        }
+                        )}
+                    </Grid>
+                    <Footer />
+                </div>
+            );
+        }
+    }
 }
